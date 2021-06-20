af47d1b6b682c1cc5af4a6bd816361374b0dd68e
junitom <juniorlocool97@gmail.com>
2021-06-19 21:24:15 -0400

    description

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
                 <img className="imageSize" src={countries} alt="Card"/>
                 <div className="card-body">
                 <h5 className="card-title">Countries App</h5>
-                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text">A React website using restcounries.eu API. Implemented with React Hooks and Material-UI to style the components and create a theme switcher.</p>
                 <div className="buttonsBox">
                 <a className='seeLive' href="https://countries-app-junior.netlify.app/" target="_blank" rel="noreferrer">
                 See Live
@@ -25,7 +25,7 @@ const Projects = () => {
                 <img className="imageSize" src={smartbrain} alt="Card"/>
                 <div className="card-body">
                 <h5 className="card-title">Face Recognition App</h5>
-                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p className="card-text">A Full-stack web application using PERN stack and Clarifai's artificial intelligence API. It finds the face in any image supplied.</p>
                 <div className="buttonsBox">
                 <a className='seeLive' href="https://supersmart-brain.herokuapp.com/" target="_blank" rel="noreferrer">
                 See Live
